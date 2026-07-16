bff8cad6f40101a2ed91a80a284c443aac7c8bf9
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2026-07-16 19:51:48 +0530

    Refactor ExploreBtn component to include event navigation and styling

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -1,9 +1,15 @@
+'use client'
+
+import Image from "next/image"
 
 const ExploreBtn = () => {
   return (
-    <button>
-      ExploreBtn
-    </button>
+      <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={() => console.log('CLICK')}>
+            <a href="#events">
+                Explore Events
+                <Image src="/icons/arrow-down.svg" alt="arrow-down" width={24} height={24} />
+            </a>
+        </button>
   )
 }
 
